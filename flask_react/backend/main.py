b8605a0aed8433835fe1d39e1c4b65e8e4df4084
mikail bhana <mikailbhana@mikails-MacBook-Pro.local>
2024-12-17 20:44:42 +0000

    adding main.py

---
+++ flask_react/backend/main.py
@@ -0,0 +1,23 @@
+import base
+
+class Movie:
+    def __init__(self, movie_name, genre, release_date):
+        self.movie_name = movie_name
+        self.genre = genre
+        self.release_date = release_date
+
+
+class Actor(Movie):
+    def __init__(self, actor_name):
+        super().__init__()
+        self.actor_name = actor_name
+
+class Director(Movie):
+    def __init__(self, director_name):
+        super().__init__()
+        self.director_name = director_name
+
+
+MyMovie = Movie('The Dark Knight', 'Action', '2008')
+MyActor = Actor('Christian Bale')
+MyDirector = Director('Christopher Nolan')
