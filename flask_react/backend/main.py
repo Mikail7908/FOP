3d35cd8154a1fc56dd8264933421c88b1f2e328e
Mikail Bhana <mikailbhanabo3@gmail.com>
2025-01-07 15:26:52 +0000

    Not in use

--- flask_react/backend/main.py
+++ flask_react/backend/main.py
@@ -1,19 +1,21 @@
-import base
+'''first iteration of classes to use in project. didnt know how to implement so changed idea'''
 
-class Movie:
-    def __init__(self, movie_name, genre, release_date):
-        self.movie_name = movie_name
-        self.genre = genre
-        self.release_date = release_date
+# import base
+
+# class Movie:
+#     def __init__(self, movie_name, genre, release_date):
+#         self.movie_name = movie_name
+#         self.genre = genre
+#         self.release_date = release_date
 
 
-class Actor(Movie):
-    def __init__(self, actor_name):
-        super().__init__()
-        self.actor_name = actor_name
+# class Actor(Movie):
+#     def __init__(self, actor_name):
+#         super().__init__()
+#         self.actor_name = actor_name
 
-class Director(Movie):
-    def __init__(self, director_name):
-        super().__init__()
-        self.director_name = director_name
+# class Director(Movie):
+#     def __init__(self, director_name):
+#         super().__init__()
+#         self.director_name = director_name
 
